98ee1697680e04082d33b9a4ee1eade7f5719976
Krisfedukfunduk <kristinkar04@mail.ru>
2024-05-11 01:30:42 +0300

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,13 @@
+import rats from './rats.png';
+function About() {
+    return (
+        <>
+            <div className="book">
+            <img src={rats} alt="книга рецептов" />
+                <span></span>
+            </div>
+        </>
+    )
+}
+
+export { About };
